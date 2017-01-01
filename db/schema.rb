e00c8eba8aab60b0c5ca414e79f049719c3f8a4e
lucky <fengzongzhixin@gmail.com>
2017-01-01 21:50:26 +0800

    add post button

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170101124534) do
+ActiveRecord::Schema.define(version: 20170101133618) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "usr_id"
     t.integer  "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
